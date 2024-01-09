912233407e12918c7ec085f3acd7de647ec6c49b
inozemcev <inozemcev>
2024-01-09 13:05:20 +0300

    bug fixes

--- Pages/Game.razor.js
+++
@@ -1,9 +0,0 @@
-export function InitAudioPlay(audioElement) {
-
-    if (audioElement) {
-        audioElement.addEventListener("canplaythrough", (e) => {
-            audioElement.play();
-        })
-    }
-    
-}
